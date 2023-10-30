68e5f3eed4b89bb46801d463b77f6e4eb75964ec
zakacoding <zakanoor@outlook.co.id>
2023-10-30 21:27:17 +0700

    add logic installation system

--- src/main.js
+++ src/main.js
@@ -8,7 +8,7 @@
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
 
-const { app, ipcMain, BrowserWindow, screen } = require('electron');
+const { app, ipcMain, BrowserWindow, screen, net } = require('electron');
 const path = require('path');
 const { spawn } = require('child_process');
 const fs = require('fs');
@@ -83,7 +83,8 @@ function createWindow() {
 
   // Load the setup.html file if it's the first setup
   if (isFirstSetup) {
-    checkInstallation();
+    // checkInstallation();
+    mainWindow.loadURL(`file://${__dirname}/index.html`);
   } else {
     // Load your main application HTML file here
     // For example: mainWindow.loadFile('path/to/your/main-app.html');
@@ -124,12 +125,17 @@ app.on('activate', function () {
   }
 });
 
-// Handle communication from the renderer process
+
+/**
+ * Handle communication from the renderer process
+ */
+
+// Check setup process
 ipcMain.on('setup-complete', () => {
   // Logic to handle setup completion, e.g., open the main window
   // You can define this logic based on your application's needs
   // For example: mainWindow.loadFile('path/to/your/main-app.html');
-  // mainWindow.loadURL(serverUrl);
+  mainWindow.loadURL(serverUrl);
 
   // Set to false to indicate that the setup is completed
   isFirstSetup = false;
@@ -138,5 +144,127 @@ ipcMain.on('setup-complete', () => {
   fs.writeFileSync(setupConfig, 'false', 'utf8');
 
   // Restart an APP
-  restartApp();
+  // restartApp();
+});
+
+// Start installation
+ipcMain.on('start-installation', (event) => {
+  // Trigger the execution of the batch file
+  const installer = path.join(__dirname, '..', 'installation.bat');
+  const installationProcess = spawn('cmd.exe', ['/c', installer]);
+
+  /**
+   * Just for development check
+   */
+  // installationProcess.stdout.on('data', (data) => {
+  //   event.reply('installation-process', `stdout: ${data}`, true);
+  // });
+
+  // installationProcess.stderr.on('data', (data) => {
+  //   event.reply('installation-status', `stderr: ${data}`, false); // mean error
+  // });
+
+  installationProcess.on('close', (code) => {
+    event.reply('installation-status', `child process exited with code ${code}`, true);
+  });
+});
+
+// Setup database on env files
+ipcMain.on('setup-database', (event, formData) => {
+  // Now, you have access to the formData object sent from the renderer process
+  const { DB_CONNECTION, DB_PORT, DB_DATABASE, DB_USERNAME, DB_PASSWORD } = formData;
+
+  // Assuming the .env file path is 'path/to/your/.env'
+  const envFilePath = path.join(__dirname, 'www', '.env');
+
+  // Read the content of the .env file
+  let content = fs.readFileSync(envFilePath, 'utf8');
+
+  // Update the content based on the configuration received
+  content = content.replace(/DB_CONNECTION=.*/, `DB_CONNECTION=${DB_CONNECTION}`);
+  content = content.replace(/DB_PORT=.*/, `DB_PORT=${DB_PORT}`);
+  content = content.replace(/DB_DATABASE=.*/, `DB_DATABASE=${DB_DATABASE}`);
+  content = content.replace(/DB_USERNAME=.*/, `DB_USERNAME=${DB_USERNAME}`);
+  content = content.replace(/DB_PASSWORD=.*/, `DB_PASSWORD=${DB_PASSWORD}`);
+
+  // Write the modified content back to the .env file
+  fs.writeFileSync(envFilePath, content, 'utf8');
+
+  event.reply('database-status', true);
+});
+
+// Migration database
+ipcMain.on('start-migration', (event) => {
+  // Trigger the execution of the batch file
+  const installer = path.join(__dirname, '..', 'migration.bat');
+  const migrationProcess = spawn('cmd.exe', ['/c', installer]);
+
+  // Set variables to track success and errors
+  let successMessage = '';
+  let errorMessage = '';
+  let migrationErrorHandled = false;
+
+  migrationProcess.stdout.on('data', (data) => {
+    successMessage += data;
+  });
+
+  migrationProcess.stderr.on('data', (data) => {
+    if (!migrationErrorHandled) {
+      migrationErrorHandled = true;
+      errorMessage += data;
+      event.reply('migration-status', errorMessage, 0); // Mean error
+    }
+  });
+
+  migrationProcess.on('close', (code) => {
+    if (code === 0) {
+      // If the process exited with code 0, it was successful
+      event.reply('migration-status', successMessage, 1); // 1 means success
+    }
+  });
+});
+
+// Check for Composer and send the result to the renderer process
+ipcMain.on('check-composer', (event) => {
+  const composerCheckProcess = spawn('where', ['composer']);
+  composerCheckProcess.on('exit', (code) => {
+    event.reply('composer-status', code === 0);
+  });
+});
+
+// Check for PHP and send the result to the renderer process
+ipcMain.on('check-php', (event) => {
+  const phpCheckProcess = spawn('where', ['php']);
+  phpCheckProcess.on('exit', (code) => {
+    event.reply('php-status', code === 0);
+  });
+});
+
+// Check for Node.js installation and send the result to the renderer process
+ipcMain.on('check-node', (event) => {
+  const nodeCheckProcess = spawn('where', ['node']);
+  nodeCheckProcess.on('exit', (code) => {
+    event.reply('node-status', code === 0);
+  });
+});
+
+// Check for internet connection and send the result to the renderer process
+ipcMain.on('check-internet', (event) => {
+  const urlToCheck = 'http://www.google.com';
+
+  const request = net.request(urlToCheck);
+
+  request.on('response', (response) => {
+    if (response.statusCode === 200) {
+      event.reply('internet-status', true);
+    } else {
+      event.reply('internet-status', false);
+    }
+  });
+
+  request.on('error', () => {
+    event.reply('internet-status', false);
+  });
+
+  request.end();
 });
